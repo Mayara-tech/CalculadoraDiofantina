0f4ef1718de9151037a14e754e99473c7d2f9109
Mayara-tech <67807684+Mayara-tech@users.noreply.github.com>
2022-12-01 21:19:49 -0300

    Add files via upload

--- Calculadora de equação diofantina/CalculadoraDiofantina/src/Calculadora/EquacaoDiofantina.java
+++ Calculadora de equação diofantina/CalculadoraDiofantina/src/Calculadora/EquacaoDiofantina.java
@@ -1,3 +1,10 @@
+
+//Calculadora de Equações Diofantinas - MD2
+//Alunos: Mayara Alves de Oliveira – 200025058 / Paulo Vinicius Pinheiro do Nascimento - 190129344
+//Disciplina FGA0108 - MATEMÁTICA DISCRETA 2
+//Professor Matheus Bernardini
+
+
 package Calculadora;
 
 import java.util.Scanner;
